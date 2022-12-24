20604a9d1f4a689190e13c09d67782687f7dfe5c
AhmadMajd9 <63857685+AhmadMajd9@users.noreply.github.com>
2022-12-24 20:05:14 +0200

    update ios info.list data

--- lib/screens/infoclient/conditinScreen.dart
+++ lib/screens/infoclient/conditinScreen.dart
@@ -5,68 +5,67 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../api/User_Controller.dart';
 import '../../../models/setting.dart';
 
-
-class ConditionScreen extends StatelessWidget{
+class ConditionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-   return Back_Ground(
-       eror: true,
-       childTab: "سياسات الاعلان",
-       child:Container(
-         margin: EdgeInsets.symmetric(horizontal: 20.w),
-         child: SingleChildScrollView(
-           child: Column(
-             // crossAxisAlignment: CrossAxisAlignment.end,
-             children: [
-
-               SizedBox(height: 24.h,),
-               Center(
-                 child: CircleAvatar(
-                     radius:77.sp,
-                     backgroundColor: Colors.white,
-                     backgroundImage: AssetImage("images/logo.png")
-
-                 ),
-               ),
-               SizedBox(height: 20.h,),
-               Center(child: Text("بوليڤارد "   ,style: TextStyle(
-                   fontWeight: FontWeight.w600,
-                   fontSize: 20.sp
-
-               ),)),
-
-               SizedBox(height: 10.h,),
-               FutureBuilder<Settings>(
-                   future: UserApiController().Setting(),
-                   builder: (context, snapshot) {
-                     if (snapshot.connectionState == ConnectionState.waiting) {
-                       return Center(
-                           child: CircularProgressIndicator(
-                             color: Colors.purple,
-                           ));
-                     } else if (snapshot.hasData) {
-
-                       return Text(snapshot.data!.advertisingPolicies.toString()
-                         ,style: TextStyle(
-                           overflow: TextOverflow.ellipsis,
-
-                         ),
-                         maxLines:80,
-                       ) ;
-                     }
-                     return Center(
-                       child: Icon(Icons.wifi_off_rounded, size: 80,color: Colors.purple,),
-                     );
-                   })
-
-
-
-             ],
-           ),
-         ),
-       )
-   );
+    return Back_Ground(
+        eror: true,
+        childTab: "سياسات الاعلان",
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20.w),
+          child: SingleChildScrollView(
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(
+                  height: 24.h,
+                ),
+                Center(
+                  child: CircleAvatar(
+                      radius: 77.sp,
+                      backgroundColor: Colors.white,
+                      backgroundImage: const AssetImage("images/logo.png")),
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Center(
+                    child: Text(
+                  "بوليڤارد ",
+                  style:
+                      TextStyle(fontWeight: FontWeight.w600, fontSize: 20.sp),
+                )),
+                SizedBox(
+                  height: 10.h,
+                ),
+                FutureBuilder<Settings>(
+                    future: UserApiController().Setting(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                            child: CircularProgressIndicator(
+                          color: Colors.purple,
+                        ));
+                      } else if (snapshot.hasData) {
+                        return Text(
+                          snapshot.data!.advertisingPolicies.toString(),
+                          style: const TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          maxLines: 80,
+                        );
+                      }
+                      return const Center(
+                        child: Icon(
+                          Icons.wifi_off_rounded,
+                          size: 80,
+                          color: Colors.purple,
+                        ),
+                      );
+                    })
+              ],
+            ),
+          ),
+        ));
   }
-
 }
